578e08eb7e50d25200326062b832dbf29eb47ad5
iq110 <iq.110@meta.ua>
2015-09-02 13:12:24 +0300

    Calculate created

---
+++ Calculate.java
@@ -0,0 +1,9 @@
+import java.lang.String;
+import java.lang.Throwable;
+
+public class Calculate{
+
+   public static void main(String[] arg) throws Throwable{
+    System.out.println("Calculate!!!");
+   }
+}
